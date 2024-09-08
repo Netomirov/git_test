74b62f20ddb59a2d1db7708e802d1b0b5fda55c9
Andrey Netomirov <netomirov@mail.ru>
2024-09-08 20:19:44 +0600

    made new feature2

--- src/main/java/Tasks.java
+++ src/main/java/Tasks.java
@@ -2,6 +2,7 @@ public class Tasks {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hello Andrey");
+        System.out.println("Hello world 2");
     }
 
 }
